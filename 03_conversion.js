8541ac12a4ee0751ebbc646debe4b3b0a09dc97b
Hitesh Soni <sonihitesh2024@gmail.com>
2024-09-13 19:45:19 +0530

    conversions

---
+++ 03_conversion.js
@@ -0,0 +1,24 @@
+let score="33abcd";
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNum=Number(score);
+console.log(valueInNum);
+console.log(typeof valueInNum);
+
+let score2=null;
+console.log(typeof score2);
+console.log(typeof(score2));
+let valueInNum2=Number(score2);
+console.log(valueInNum2);
+console.log(typeof valueInNum2);
+
+//if any variable convertable in number then it will convert in respective type of variable 
+//but it is not convertable then it will shows NaN
+let isLoggedIn2=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn2);
+console.log(booleanIsLoggedIn);
+console.log(typeof isLoggedIn2);
+console.log(typeof booleanIsLoggedIn);
+
+
+ 
