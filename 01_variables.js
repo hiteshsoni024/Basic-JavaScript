56386cc8b92e2f969f5ce75fad35c1574da7db66
Hitesh Soni <sonihitesh2024@gmail.com>
2024-09-13 15:42:38 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId=114450
+let accountEmail="hitesh@google.com"
+var accountPassword="1234"
+accountCity="jaipur"
+
+// accountId=2 constant is unchangable in entier code
+accountEmail="hs@google.com"
+accountPassword="3128"
+accountCity="bengluru"
+console.log(accountId);
+console.log();
