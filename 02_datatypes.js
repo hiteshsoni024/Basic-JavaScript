5140f8d9c96b154d9c897648b431ba7f9a848ef6
Hitesh Soni <sonihitesh2024@gmail.com>
2024-09-13 17:20:38 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as a newr version
+//alert(3+3) //we are using node.js , not browser(for this chrome is required)
+//code readablity is must
+let name= "Hitesh soni"//string
+let age= 21//number
+let isLoggedIn= true//boolean
+//null => standlone value(Empty value)
+//undifined
+//symbol => unique
+
+//object
+console.log(typeof"Hitesh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
