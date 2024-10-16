790dbe5f2b5a44fe9de36fd1241bd2b4985aa4de
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-10-16 11:17:53 +0530

    modified

--- 05_comparison.js
+++ 05_comparison.js
@@ -16,3 +16,9 @@ console.log(null>=0);
 console.log(undefined==0);
 console.log(undefined>0);
 console.log(undefined<0);
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+console.log(id);
+console.log(anotherId);
+console.log(id==anotherId);
