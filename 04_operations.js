70d259513732ed9137224335f19a1c1b4433f3fe
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-10-16 10:53:35 +0530

    operations

---
+++ 04_operations.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue= -value
+console.log(negValue);
+
+console.log(2+2); //Add
+console.log(2-2);//sub
+console.log(2*3);//multiply
+console.log(2/3);//divide
+console.log(2**3);//power
+console.log(2%5);//remender
+// *****string concatination******
+let str1 ="Hello"
+let str2=" Hitesh"
+console.log(str1+str2);
+console.log("1"+2);//=>12
+console.log(1+"2");//=>12
+console.log("1"+2+2);//=>122
+console.log(3+2+1+"2");//=>62
+
+/*prefix and postfix */
+let x=3;
+const y = x++;
+console.log(`x:${x}  y:${y} `); //x:4 y:3
+//post increament
+let a = 4;
+const b= ++a;
+console.log(`a:${a} b:${b}`); //a:5 b:5
+//preincreament
