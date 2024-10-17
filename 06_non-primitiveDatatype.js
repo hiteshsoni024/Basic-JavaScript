1349e50eed0de60b990389d0c3afe1a7c7130149
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-10-17 12:44:29 +0530

    17 oct

--- 06_non-primitiveDatatype.js
+++ 06_non-primitiveDatatype.js
@@ -1,11 +1,12 @@
 //non-primitive data types : Array , objects, Functions
 
 const heros=["ironman","hulk","thor"];//array
+
 let myObj ={
     name: "Hitesh",  //object
     age: 20
 }
 
 const myFunc= function(){
-    console.log("Jai shree Ram");
+    console.log("Jai shree Ram");  //function
 }
