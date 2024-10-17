8f192281b1ba9d133ac11d615167b30a6048a6b1
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-10-17 14:08:00 +0530

    17 oct

--- 06_non-primitiveDatatype.js
+++ 06_non-primitiveDatatype.js
@@ -10,3 +10,23 @@ let myObj ={
 const myFunc= function(){
     console.log("Jai shree Ram");  //function
 }
+
+/*Stack and Heap memory in javascript */
+//Stack memory : primitive data types , function call stack
+//Heap memory : non-primitive data types , objects , arrays , functions
+
+// stack memory
+let myName="Hitesh Soni"
+let anotherName = myName
+anotherName = "Soni Hitesh"
+ console.log(myName)
+ console.log(anotherName)
+
+//heap memory
+let user={
+    email: "username@gmail.com",
+    name: "userName",
+    upi: "user@ybl"
+}
+console.log(user.email);
+console.log(user.upi);
---
+++ 07_Strings.js
@@ -0,0 +1,5 @@
+const name ="Hitesh "
+const repoCount =2
+// console.log("Hello i am "+name+repoCount+" I am learning Javascript");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
