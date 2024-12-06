076ed861ad9c22028e3ab063c86c965336dc3ce3
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-12-06 16:21:56 +0530

    6dec

--- 01_variables.js
+++ 01_variables.js
@@ -1,11 +1,17 @@
-const accountId=114450
+const accountId=114450 
 let accountEmail="hitesh@google.com"
-var accountPassword="1234"
+var accountPassword="1234" //
 accountCity="jaipur"
-let accountState;
+let accountState; 
+
+/* prefer not to use var
+becoz of issue in block scope and functional scope
+*/
 
 //this is posible to assign value without any assigning variable but it is not prefer to use
+
 // accountId=2 constant is unchangable in entier code
+//accountID is a constant so we can't change it
 accountEmail="hs@google.com"
 accountPassword="3128"
 accountCity="bengluru"
@@ -13,8 +19,4 @@ console.log(accountId);
 console.log(accountEmail);
 console.log(accountPassword);
 console.log(accountCity);
-/* prefer not to use var
-becoz of issue in block scope and functional scope
-*/
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
-
