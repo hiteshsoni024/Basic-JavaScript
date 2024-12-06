a621eb41842b92fd905c0a285e6c93d118eece77
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-12-07 00:47:40 +0530

    7 dec

--- 03_conversion.js
+++ 03_conversion.js
@@ -1,5 +1,5 @@
-let score="33abcd";
-console.log(typeof score);
+let score="33abcd"; 
+console.log(typeof score);  
 console.log(typeof(score));
 let valueInNum=Number(score);
 console.log(valueInNum);
@@ -14,11 +14,15 @@ console.log(typeof valueInNum2);
 
 //if any variable convertable in number then it will convert in respective type of variable 
 //but it is not convertable then it will shows NaN
-let isLoggedIn2=1;
+let isLoggedIn2=1; 
 let booleanIsLoggedIn=Boolean(isLoggedIn2);
 console.log(booleanIsLoggedIn);
 console.log(typeof isLoggedIn2);
 console.log(typeof booleanIsLoggedIn);
 
+let someNum=22;
+let StringNum=String(someNum);
+console.log(StringNum);
+console.log(typeof(StringNum));
 
  
