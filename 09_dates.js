75ca414e9e5af355a79de297ab81b515717c8276
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-12-07 21:41:26 +0530

    date and time

---
+++ 09_dates.js
@@ -0,0 +1,7 @@
+let myDate= new Date();
+console.log(myDate);//2024-12-07T08:23:20.187Z
+console.log(myDate.toString());//Sat Dec 07 2024 13:54:00 GMT+0530 (India Standard Time)
+console.log(myDate.toDateString());//Sat Dec 07 2024
+console.log(myDate.toISOString());//2024-12-07T08:30:55.757Z
+console.log(myDate.toJSON());//2024-12-07T08:30:55.757Z
+console.log(myDate.toLocaleDateString());//7/12/2024
