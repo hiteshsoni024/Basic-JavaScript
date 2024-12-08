100c9b4319e65b3a68982b516edfcebde70e7da4
hiteshsoni024 <sonihitesh2024@gmail.com>
2024-12-08 10:08:47 +0530

    dates and time

--- 09_dates.js
+++ 09_dates.js
@@ -6,5 +6,19 @@ console.log(myDate.toISOString());//2024-12-07T08:30:55.757Z
 console.log(myDate.toJSON());//2024-12-07T08:30:55.757Z
 console.log(myDate.toLocaleDateString());//7/12/2024
 //date is object
+
 let NewDate = new Date(2025,0,21);
 console.log(NewDate.toDateString());
+
+let TimeStamp = Date.now();
+console.log(TimeStamp);//gives time stamp in milliseconds
+console.log(NewDate.getTime());
+console.log(Date.now());
+
+
+console.log (NewDate.toLocaleDateString('default',{
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+ }))
