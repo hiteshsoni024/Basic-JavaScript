e20c14125aaee708dbfdcdef0f5586c4249b91eb
Hitesh Soni <163199753+hiteshsoni024@users.noreply.github.com>
2024-12-08 05:22:11 +0000

    array2

---
+++ 11_Arrays.js
@@ -0,0 +1,5 @@
+const marvelHeros=["ironman","hulk","thor"];
+const DCheros= ["superman","flash","batman"];
+marvelHeros.push(DCheros);
+console.log(marvelHeros);
+console.log(DCheros);//array takes entire DCheros as a element in marvelHeros 
