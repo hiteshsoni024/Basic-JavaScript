50b99acef3fe1fe0727caaf8651912db55b9a684
hiteshsoni024 <sonihitesh2024@gmail.com>
2025-01-14 19:08:54 +0530

    error resolution

--- 19_IIFE.js
+++ 19_IIFE.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-const user={
-    name: "John Doe",
-    age: 30,
-    welcomegreeting: function() {
-    console.log(`${this.name} , welcome to website`);
-    // console.log(this);//this tells about current context
-    }
-
-}
-// user.welcomegreeting()
-// user.name = "Hitsh Soni";
-// user.welcomegreeting();
-// user.name="Tarachand soni"
-// user.welcomegreeting()
-console.log(this);//we are in node environment -> global content is empty that's why returns empty object
-
-// function onechai(){
-//     let username ="Hitesh"
-//     console.log(this.username);//undefined
-// }
-// onechai() 
-
-/*Arrow function */
-// const chai = () =>{
-//     let username ="Hitesh"
-//         console.log(this.username);//undefined
-// }
-// chai()
-
-// const addnum=(num1,num2)=>{
-//     return num1 + num2;
-// }
-// const addnum = (num1,num2)=> num1+num2 //this will run
-// const addnum =(num1,num2)=> (num1+num2)//this will run
-// const addnum =(num1,num2)=> {num1,num2}//this will not run untill we use return
-const addnum =(num1,num2)=> {username : "Hitesh"}//this will not run untill if we not use () because this is object
-console.log(addnum(3,5));
-=======
 //Immediately invoke fuction expression (IIFE)
 //They are typically used to create a local scope for variables to prevent them from polluting the global scope.
 ( function chai(){//named IIFE
@@ -48,4 +9,3 @@ console.log(addnum(3,5));
 ( (name) => {//unnamed IIFE
     console.log(`DataBase connected ! ${name}`);
 } )("Hitesh");//parameter is passes through name
->>>>>>> 7a9985e1bb2074c270214b45d779a8853912ab1f
